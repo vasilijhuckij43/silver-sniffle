7f9dc141735a49e413ae22b43e4426a3534829b1
Mary608 <151008728+vasilijhuckij43@users.noreply.github.com>
2023-12-28 02:01:20 +0700

    Create silver-sniffle.swift

---
+++ silver-sniffle.swift
@@ -0,0 +1,11 @@
+   public function getUsername() {
+        return $this->username;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function isLoggedIn() {
+        return $this->isLoggedIn;
+    }
